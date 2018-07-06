d6ae30ebaaeb02772731f3c94bb7cc514f9b1a89
Andie <zchangan@163.com>
2018-07-06 18:27:13 +0800

    update tfrecord creator

--- tfrecord_creator.py
+++ tfrecord_creator.py
@@ -1,13 +1,15 @@
-import tensorflow as tf
 import os
-import dataset_util
-import numpy as np
-import random
+import logging
 import argparse
+import numpy as np
+import dataset_util
+import tensorflow as tf
+
+LOG_FORMAT = "[%(asctime)s] [%(levelname)s] - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--sample', default=0, type=int, help='sample size')
-parser.add_argument('--predict', default=None, nargs='+', type=str, help='specific image to predict, can be all or random')
+parser.add_argument('--predict', default=False, type=bool, help='write predict.tfrecord')
 
 _TRAIN_RATE = 0.85
 
@@ -32,12 +34,9 @@ def make_example(filename, label, ymin, xmin, ymax, xmax):
 
 def main(argv):
     args = parser.parse_args(argv[1:])
+
     print('reading bbox...')
-    bbox_raw = np.loadtxt(
-        './data/Anno/list_bbox_inshop.txt', skiprows=2, dtype='str')
-    if args.predict is None:
-        print('shuffling...')
-        np.random.shuffle(bbox_raw)
+    bbox_raw = np.loadtxt('./data/Anno/list_bbox_inshop.txt', skiprows=2, dtype='str')
 
     print('reading attributes...')
     attr = np.loadtxt('./data/Anno/list_attr_items.txt', skiprows=2, dtype=str)
@@ -45,18 +44,26 @@ def main(argv):
     labels = attr[:,1:].astype(np.int)
     labels = np.vectorize(lambda x: 0 if x == -1 else 1)(labels)
 
+    if not args.predict:
+        print('shuffling...')
+        np.random.shuffle(bbox_raw)
+    
     print('writting...')
     if args.predict is None:
         train_writer = tf.python_io.TFRecordWriter('./data/train.tfrecord')
         test_writer = tf.python_io.TFRecordWriter('./data/test.tfrecord')
     else:
         pred_writer = tf.python_io.TFRecordWriter('./data/predict.tfrecord')
-        if args.predict[0] == 'random':
-            args.predict = [random.choice(bbox_raw)[0]]
-            print('random', args.predict)
 
     total_num = len(bbox_raw)
     for i in range(total_num):
+        if args.predict:
+            writer = pred_writer
+        elif i < total_num * _TRAIN_RATE:
+            writer = train_writer
+        else:
+            writer = test_writer
+
         bbox = bbox_raw[i]
         filename = bbox[0]
         foundIndex = filename.find('id_') + 3
@@ -66,30 +73,14 @@ def main(argv):
         ymin = int(bbox[4])
         xmax = int(bbox[5])
         ymax = int(bbox[6])
-        if args.predict is not None:
-            if args.predict[0] != 'all' and filename not in args.predict:
-                continue
 
         example = make_example(filename, label, ymin, xmin, ymax, xmax)
 
-        if (args.sample > 0):
-            if (i < args.sample):
-                train_writer.write(example.SerializeToString())
-                test_writer.write(example.SerializeToString())
-            else:
-                break
-            continue
-
-        if args.predict is not None:
-            pred_writer.write(example.SerializeToString())
-        elif (i < total_num * _TRAIN_RATE):
-            train_writer.write(example.SerializeToString())
-        else:
-            test_writer.write(example.SerializeToString())
+        writer.write(example.SerializeToString())
         if (i % 1000 == 0):
             print(i, 'done')
 
-    if args.predict is None:
+    if not args.predict:
         train_writer.close()
         test_writer.close()
         train_num = sum(1 for _ in tf.python_io.tf_record_iterator(
